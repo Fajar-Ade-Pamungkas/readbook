ee1fb382b0d21b8343dc975ff6408e6aa5641000
Fajar Ade Pamungkas <fajaradepamungkas98@gmail.com>
2022-02-01 21:55:15 +0700

    menambahkan banyak file di root directory

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php 
+   $arr = [
+   				"Andi Oka Mahendra",
+   				"Iqbal Abdillah",
+   				"Laila Syifaurrahmah",
+   				"Rizki Ade Pangestu",
+   				"Fajar Ade Pamungkas"
+   ];
+   
+   $hur = [
+   				"a",
+   				"b",
+   				"c",
+   				"d",
+   				"e"
+   ];
+?>
+
+<!DOCTYPE html>
+<html lang="in">
+<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Judul halaman</title>
+</head>
+<body>
+				<h1>Childhood</h1> 
+				<?php 
+				   $n = 5;
+				   $array1 = [];
+				   
+				   for($i = 0; $i < $n; $i++) {
+				   				if($i == 0 && $i == 1) {
+				   								$array1[$i] = $i;
+				   				} else {
+				   								$array1[$i] = ;
+				   				}
+				   				echo "$array1[$i]";
+				   }
+				?>
+</body>
+</html>
